ea74b7c55e176f769f8c527b1bb3d53eeabd04f6
NicoTapia45 <102261383+NicoTapia45@users.noreply.github.com>
2023-07-05 21:42:07 -0400

    Update OperacionesMatriz.java
    Se agrega comentario de uso de la clase para JavaDoc

--- OperacionesMatriz.java
+++ OperacionesMatriz.java
@@ -1,3 +1,7 @@
+/**
+ * Esta interfaz define las operaciones que se pueden realizar en una matriz.
+ * Estas operaciones incluyen suma, resta, multiplicación e inversa.
+ */
 public interface OperacionesMatriz {
     Matriz sumar(Matriz otra);
     Matriz restar(Matriz otra);
